914ea93477f66cb4210c9f0d172ceed87da3873a
Matt J Owen <matt.owen@nottingham.ac.uk>
2023-07-06 19:06:54 +0100

    artefact modelling
    added fink 2008 artefact (simplified) and artefact free models, general fitting code, voltageClamp script to generate synthetic data and then fit to the artefact data
    Outputs are saved with filenames beginning with test which are ignored from git

---
+++ Code/myokitFitting.py
@@ -0,0 +1,143 @@
+#%%
+# Imports
+import matplotlib.pyplot as plt
+from Code import myokitGeneral
+import numpy as np
+import pints
+
+class Model(pints.ForwardModel):
+    def __init__(self, modelName, paramNames, protocol, outputName):
+        self.model = myokitGeneral.loadModel(modelName)
+        self.sim = myokitGeneral.compileModel(self.model)
+        self.sim, self.tmax = myokitGeneral.addProtocol(self.sim, protocol)
+        
+        self.paramNames = paramNames
+        self.outputName = outputName
+        
+        self.sim.pre(200)
+        
+    def n_parameters(self):
+        return len(self.paramNames)
+    
+    def simulate(self, parameters, times):
+        # Reset the simulation
+        self.sim.reset()
+        
+        # Update the parameters
+        self.sim = myokitGeneral.editSim(self.sim, factorVars = self.paramNames, factorVals = parameters)
+        
+        # Run a simulation
+        output = myokitGeneral.simulate(self.sim, self.tmax, output = [self.outputName])
+        
+        return output[0]
+
+class AdvancedBoundaries(pints.Boundaries):
+    def __init__(self, paramCount, localBounds, kCombinations, vHigh = 40, vLow = -120):
+        # localBounds = [[0,1e-7,1e3],[3,1e-3,1e5]] sets the bounds for parameter index 0 to be [1e-7,1e3] and index 3 to be [1e-3,1e5]
+        # kCombinations = [[0,1],[4,5]] means param[0]*exp(param[1]*V) and param[4]*exp(param[5]*V) satisfy bounds
+        # self.a_min = 1e-7
+        # self.a_max = 1e3
+        # self.b_min = 1e-7
+        # self.b_max = 0.4
+        self.km_min = 1.67e-5
+        self.km_max = 1e3
+        self.vLow = vLow
+        self.vHigh = vHigh
+        self.paramCount = paramCount
+        self.kCombinations = kCombinations
+        
+        self.lowerBounds = [-np.inf for i in range(paramCount)]
+        self.upperBounds = [np.inf for i in range(paramCount)]
+        for bound in localBounds:
+            self.lowerBounds[bound[0]] = bound[1]
+            self.upperBounds[bound[0]] = bound[2]
+    
+    def n_parameters(self):
+        return self.paramCount
+    
+    def check(self, parameters):
+        
+        # Check parameter boundaries
+        if np.any(parameters <= self.lowerBounds) or np.any(parameters >= self.upperBounds):
+            return False
+        
+        for comb in self.kCombinations:
+            kLow = parameters[comb[0]] * np.exp(parameters[comb[1]] * self.vLow)
+            kHigh = parameters[comb[0]] * np.exp(parameters[comb[1]] * self.vHigh)
+            if kLow < self.km_min or kHigh < self.km_min or kLow > self.km_max or kHigh > self.km_max:
+                return False
+        
+        # All tests passed!
+        return True
+
+def fitting(model, times, values, parameters, iterCount, localBounds, kCombinations, logTransforms, maxIter = 10000, returnAll = False, plotting = False):
+    problem = pints.SingleOutputProblem(model, times, values)
+    error = pints.MeanSquaredError(problem)
+    boundaries = AdvancedBoundaries(paramCount = model.n_parameters(), localBounds = localBounds, kCombinations = kCombinations)
+    
+    for i in range(len(parameters)):
+        if i == 0:
+            if i in logTransforms:
+                transformation = pints.LogTransformation(n_parameters=1)
+            else:
+                transformation = pints.IdentityTransformation(n_parameters=1)
+        else:
+            if i in logTransforms:
+                transformation = pints.ComposedTransformation(transformation, pints.LogTransformation(n_parameters=1))
+            else:
+                transformation = pints.ComposedTransformation(transformation, pints.IdentityTransformation(n_parameters=1))
+    if plotting:
+        plt.figure(figsize=(16, 5))
+        plt.xlabel('Time (ms)')
+        plt.ylabel('Current (nA)')
+        plt.plot(times, values, label='Data')
+        plt.plot(times, problem.evaluate(parameters), label='Default parameters')
+        plt.legend(loc='upper left')
+        plt.show()
+    
+    xbests = np.zeros((iterCount,model.n_parameters()))
+    fbests = np.zeros((iterCount))
+    for i in range(iterCount):
+        x0 = parameters * 2**np.random.normal(0, 0.5, len(parameters))
+        while not boundaries.check(x0):
+            x0 = parameters * 2**np.random.normal(0, 0.5, len(parameters))
+        # Create an optimisation controller
+        opt = pints.OptimisationController(error, x0, boundaries=boundaries, transformation=transformation, method=pints.CMAES)
+        opt.set_max_iterations(maxIter)
+        # Run the optimisation
+        xbest, fbest = opt.run()
+        xbests[i,:] = xbest
+        fbests[i] = fbest
+        
+        if plotting:
+            # Show the final result
+            plt.figure(figsize=(16, 5))
+            plt.xlabel('Time (ms)')
+            plt.ylabel('Current (nA)')
+            plt.plot(times, values, label='Noisy data')
+            plt.plot(times, problem.evaluate(xbest), label='Fit')
+            plt.legend(loc='upper left')
+            plt.show()
+
+    i = np.argmin(fbests)
+    xbest = xbests[i,:]
+    fbest = fbests[i]
+
+    print('Best rates found. f value of ')
+    print(fbest)
+    print('found at parameters')
+    print(xbest)
+    
+    if plotting:
+        # Show the final result
+        plt.figure(figsize=(16, 5))
+        plt.xlabel('Time (ms)')
+        plt.ylabel('Current (nA)')
+        plt.plot(times, values, label='Noisy data')
+        plt.plot(times, problem.evaluate(xbest), label='Best fit')
+        plt.legend(loc='upper left')
+        plt.show()
+    if returnAll:
+        return xbest, fbest, xbests, fbests
+    else:
+        return xbest, fbest
---
+++ Code/voltageClampArtefactModelling.py
@@ -0,0 +1,128 @@
+import os
+if os.getcwd()[-4:] == 'Code':
+    os.chdir('..')
+import numpy as np
+import matplotlib.pyplot as plt
+from Code import myokitGeneral, myokitFitting
+from datetime import datetime
+import csv
+
+#Options
+plotting = False
+filenameOut = 'test-001'
+
+def fit_leak_lr(staircase_protocol, current, V_win=[-115, -85], V_full=[-120, -80],
+        ramp_start=0.3, ramp_end=0.7, dt=2e-4):
+    # Fitting leak during the first ramp in staircaseramp prt
+    #
+    # staircase_protocol: full staircase ramp protocol
+    # current: corresponding current for the staircase ramp protocol
+    # V_win: Voltage window for fitting (in the direction of time)
+    # V_full: Full voltage range during the ramp (in the direction of time)
+    # ramp_start: starting time of the ramp that matches the input protocol
+    # ramp_end: ending time of the ramp that matches the input protocol
+    # dt: duration of each time step to work out the index in the input protocol
+    from scipy import stats
+    rampi, rampf = int(ramp_start / dt), int(ramp_end / dt)
+    n_samples = rampf - rampi
+    idxi = int(np.abs(V_win[0] - V_full[0])\
+            / np.abs(V_full[1] - V_full[0])\
+            * n_samples)
+    idxf = int(np.abs(V_win[1] - V_full[0])\
+            / np.abs(V_full[1] - V_full[0])\
+            * n_samples)
+    # Assumed V_win, V_full where given correctly!!
+    x = staircase_protocol[rampi:rampf][idxi:idxf]
+    y = current[rampi:rampf][idxi:idxf]
+    slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
+    return slope, -1 * intercept / slope  # g_leak, E_leak
+
+np.random.seed(1)
+print(datetime.now())
+
+voffset = np.random.normal(0,1.5)
+gLeak = np.random.normal(0.25,0.1)*20
+gKr = 21.53/2 + np.random.lognormal(2.723,0.83255)
+
+print('Voltage Offset (mV): '+str(voffset))
+print('Leak conductance (nS): '+str(gLeak))
+print('gKr (nS): '+str(gKr))
+
+with open(filenameOut+'-trueParams.csv', 'w', newline = '') as csvfile:
+    writer = csv.writer(csvfile, delimiter = ',')
+    writer.writerow(['voffset', 'gLeak', 'gKr'])
+    writer.writerow([voffset, gLeak, gKr])
+
+model = myokitGeneral.loadModel('fink2008-artefactModel')
+sim = myokitGeneral.compileModel(model, factorVars = ['voltageclamp.voffset_eff', 'voltageclamp.gLeak', 'iKr_Markov.p17'], factorVals = [voffset, gLeak, gKr])
+sim, tmax = myokitGeneral.addProtocol(sim, 'Additional Protocols\staircase-ramp')
+
+output = myokitGeneral.simulate(sim, tmax, output = ['environment.Imeasured', 'environment.IKr', 'membrane.V','voltageclamp.Vc','voltageclamp.ILeak_est'])
+
+if plotting:
+    plt.plot(output[0])
+    plt.title('Measured Current')
+    plt.show()
+    plt.plot(output[1])
+    plt.title('IKr current')
+    plt.show()
+    plt.plot(output[2])
+    plt.title('Membrane Voltage')
+    plt.show()
+    plt.plot(output[4])
+    plt.title('Estimated Leakage Current')
+    plt.show()
+
+gLeak_est, ELeak_est = fit_leak_lr(output[3],output[0],dt=1e-3)
+print('Estimated Leak Conductance: '+str(gLeak_est))
+print('Estimated Leak Reversal Potential: '+str(ELeak_est))
+sim.reset()
+sim = myokitGeneral.editSim(sim, factorVars = ['voltageclamp.voffset_eff','voltageclamp.gLeak','iKr_Markov.p17', 'voltageclamp.gLeak_est','voltageclamp.ELeak_est'], factorVals = [voffset, gLeak, gKr, gLeak_est, ELeak_est])
+
+outputArtefact = myokitGeneral.simulate(sim, tmax, output = ['environment.Imeasured', 'environment.IKr', 'membrane.V','voltageclamp.Vc','voltageclamp.ILeak_est'])
+leakSubtractedCurrent = np.subtract(output[0],output[4])
+
+if plotting:
+    plt.plot(leakSubtractedCurrent)
+    plt.title('Measured Current: Leak Subtracted')
+    plt.show()
+
+model = myokitGeneral.loadModel('fink2008-artefactFreeModel')
+sim = myokitGeneral.compileModel(model)
+sim, tmax = myokitGeneral.addProtocol(sim, 'Additional Protocols\staircase-ramp')
+
+outputArtefactFree = myokitGeneral.simulate(sim, tmax, output = ['environment.Imeasured', 'membrane.V'])
+
+with open(filenameOut+'-dataCurrents.csv', 'w', newline = '') as csvfile:
+    writer = csv.writer(csvfile, delimiter = ',')
+    writer.writerow(['time', 'artefactCurrent', 'artefactFreeCurrent'])
+    writer.writerows(tuple(zip(np.arange(tmax), leakSubtractedCurrent, outputArtefactFree[0])))
+
+#%% Fitting
+modelName = 'fink2008-artefactModel'
+paramNames = ['iKr_Markov.p1', 'iKr_Markov.p2', 'iKr_Markov.p3', 'iKr_Markov.p5', 'iKr_Markov.p6', 'iKr_Markov.p7', 'iKr_Markov.p8', 'iKr_Markov.p9', 'iKr_Markov.p10', 'iKr_Markov.p11', 'iKr_Markov.p13', 'iKr_Markov.p14', 'iKr_Markov.p15', 'iKr_Markov.p16', 'iKr_Markov.p17', 'voltageclamp.voffset_eff', 'voltageclamp.gLeak']
+protocol = 'Additional Protocols\staircase-ramp'
+outputName = 'environment.Imeasured'
+model = myokitFitting.Model(modelName, paramNames, protocol, outputName)
+
+times = np.arange(0,tmax)
+values = output[0]
+parameters = [0.20618, 0.0112, 0.04209, 0.02202, 0.0365, 0.41811, 0.0223, 0.13279, -0.0603, 0.08094, 0.00023, -0.0399, 0.04150, -0.0312, 0.1524*1e3, 0, 5]
+parameters = np.multiply(parameters,np.random.uniform(0.5,1.5,len(parameters)))
+iterCount = 5
+localBounds = [[0,1e-7,1e3], [1,1e-7,0.4], [2,1e-7,1e3], [3,1e-7,1e3], [4,1e-7,0.4], [5,1e-7,1e3], [6,1e-7,0.4], [7,1e-7,1e3], [8,-0.4,-1e-7], [9,1e-7,1e3], [10,1e-7,1e3], [11,-0.4,-1e-7], [12,1e-7,1e3], [13,-0.4,-1e-7]]
+kCombinations = [[0,1], [3,4], [5,6], [7,8], [10,11], [12,13]]
+logTransforms = [0, 2, 3, 5, 7, 9, 10, 12]
+
+xbest, fbest, xbests, fbests = myokitFitting.fitting(model, times, values, parameters, iterCount, localBounds, kCombinations, logTransforms, returnAll = True, plotting = plotting, maxIter = 2500)
+
+with open(filenameOut+'-fittedParams.csv', 'w', newline = '') as csvfile:
+    writer = csv.writer(csvfile, delimiter = ',')
+    writer.writerow(['paramNames','initialParams', 'bestParams-f-'+str(fbest)])
+    writer.writerows(tuple(zip(paramNames, parameters, xbest)))
+
+with open(filenameOut+'-posteriors.csv', 'w', newline = '') as csvfile:
+    writer = csv.writer(csvfile, delimiter = ',')
+    writer.writerow(['paramNames'] + ['params-f-'+str(fbests[i]) for i in range(iterCount)])
+    a = [list(i) for i in zip(*xbests)]
+    writer.writerows([[paramNames[i]] + a[i] for i in range(len(paramNames))])
